8ad551db461381aa3f1a1f5d17347460e36d6e84
beautus <gumedesduduzo@gmail.com>
2021-08-14 20:25:08 +0200

    revert stuff

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { createApp } from 'vue';
-import { createSupabase } from 'vue-supabase';
+import VueSupabase from 'vue-supabase';
 import App from './App.vue';
 import './index.css';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '/@/views/home.vue';
 
 const routes = [{ path: '/', component: Home }];
-
-const supabase = createSupabase({
-  supabaseUrl: 'https://glcenwzasztexldnfmud.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNDAzMzE5MCwiZXhwIjoxOTM5NjA5MTkwfQ.Pfzgv4VIMs2_Wm5okKXzsgIbGJ4ROXtPDiecpHmnOUw',
-});
 
 const router = createRouter({
   routes,
@@ -21,6 +15,10 @@ const router = createRouter({
 const app = createApp(App);
 
 app.use(router);
-app.use(supabase);
+app.use(VueSupabase, {
+  supabaseUrl: 'https://glcenwzasztexldnfmud.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNDAzMzE5MCwiZXhwIjoxOTM5NjA5MTkwfQ.Pfzgv4VIMs2_Wm5okKXzsgIbGJ4ROXtPDiecpHmnOUw',
+});
 
 app.mount('#app');
