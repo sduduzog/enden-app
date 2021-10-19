dd9f2c41970abd3a73f20ed434620f2bc7bb038d
beautus <gumedesduduzo@gmail.com>
2021-10-19 10:08:18 +0200

    layout the landing page

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,15 +2,18 @@
   <div
     class="
       min-h-screen
-      bg-gradient-to-r
+      bg-gradient-to-br
+      md:bg-gradient-to-r
       to-indigo-400
       from-fuchsia-400
       via-rose-400
-      flex
+      flex flex-wrap flex-row-reverse
+      items-start
+      md:items-stretch
     "
   >
-    <div class="flex-grow"></div>
-    <div class="w-full max-w-lg bg-white"></div>
+    <div class="w-full md:max-w-xl bg-white">Trees</div>
+    <div class="flex-grow">food</div>
   </div>
 </template>
 <script setup></script>
