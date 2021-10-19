eb8ada958b78101fbb0fccdaf4e674931249b186
beautus <gumedesduduzo@gmail.com>
2021-10-19 20:03:50 +0200

    add font and twitter component

--- src/views/index.vue
+++ src/views/index.vue
@@ -7,13 +7,51 @@
       to-indigo-400
       from-fuchsia-400
       via-rose-400
-      flex flex-wrap flex-row-reverse
+      md:flex md:flex-wrap
+      flex-row-reverse
       items-start
       md:items-stretch
     "
   >
-    <div class="w-full md:max-w-xl bg-white">Trees</div>
-    <div class="flex-grow">food</div>
+    <div
+      class="
+        w-full
+        min-h-screen
+        md:min-h-full md:max-w-xl
+        bg-white
+        p-8
+        pt-16
+        md:pt-28
+        space-y-8
+        md:space-y-16
+      "
+    >
+      <h1
+        class="
+          text-6xl
+          font-black
+          text-transparent
+          bg-gradient-to-r bg-clip-text
+        "
+      >
+        Manage tasks with your budget in mind.
+      </h1>
+      <button
+        style="background-color: rgb(29, 161, 242)"
+        class="rounded-md text-white p-2 px-4 flex space-x-3 items-center"
+      >
+        <twitter-logo class="w-6" />
+        <span>Sign-In with Twitter</span>
+      </button>
+    </div>
+    <div class="hidden flex-grow">food</div>
   </div>
 </template>
-<script setup></script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import twitterLogo from '~/components/twitter-logo.vue';
+
+export default defineComponent({
+  components: { twitterLogo },
+});
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 const colors = require('tailwindcss/colors');
+const defaultConfig = require('tailwindcss/defaultConfig');
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
+      transparent: 'transparent',
       white: colors.white,
       gray: colors.blueGray,
       fuchsia: colors.fuchsia,
       rose: colors.rose,
       indigo: colors.indigo,
+    },
+    fontFamily: {
+      sans: ['Inter', ...defaultConfig.theme.fontFamily.sans],
     },
     extend: {},
   },
