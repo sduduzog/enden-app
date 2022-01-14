2506141f6a87eff167df40a8dba9d7595d23818b
beautus <gumedesduduzo@gmail.com>
2022-01-15 01:41:51 +0200

    wip

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,59 +1,22 @@
 <template>
   <div
-    class="
-      min-h-screen
-      bg-gradient-to-br
-      md:bg-gradient-to-r
-      to-indigo-400
-      from-fuchsia-400
-      via-rose-400
-      md:flex md:flex-wrap
-      flex-row-reverse
-      items-start
-      md:items-stretch
-    ">
+    class="flex-row-reverse items-start min-h-screen bg-gradient-to-br md:bg-gradient-to-r to-indigo-400 from-fuchsia-400 via-rose-400 md:flex md:flex-wrap md:items-stretch">
     <div
-      class="
-        w-full
-        min-h-screen
-        md:min-h-full md:max-w-screen-md
-        bg-white
-        p-8
-        md:pt-20
-        space-y-8
-        md:space-y-8
-      ">
-      <h1 class="text-3xl font-black text-fuchsia-600 py-8">enden?</h1>
+      class="w-full min-h-screen p-8 space-y-8 bg-white md:min-h-full md:max-w-screen-md md:pt-20 md:space-y-8">
+      <h1 class="py-8 text-3xl font-black text-fuchsia-600">enden?</h1>
       <h1
-        class="
-          text-6xl
-          font-black
-          text-transparent
-          bg-gradient-to-r bg-clip-text
-          py-2
-        ">
+        class="py-2 text-6xl font-black text-transparent bg-gradient-to-r bg-clip-text">
         Manage tasks with your budget in mind.
       </h1>
       <div v-if="loading">
-        <span class="inline-block w-44 p-2 px-4 text-center font-medium">
+        <span class="inline-block p-2 px-4 font-medium text-center w-44">
           Loading...
         </span>
       </div>
       <button
         v-else
         style="background-color: rgb(29, 161, 242)"
-        class="
-          rounded-md
-          overflow-hidden
-          hover:opacity-80
-          flex
-          items-center
-          w-60
-          p-2
-          px-4
-          space-x-3
-          text-white
-        "
+        class="flex items-center p-2 px-4 space-x-3 overflow-hidden text-white rounded-md hover:opacity-80 w-60"
         :class="{ 'grayscale opacity-40': loginDisabled }"
         :disabled="loginDisabled"
         @click="signInWithTwitter">
@@ -63,15 +26,18 @@
       <div class="flex flex-wrap items-end w-80">
         <input
           type="text"
-          class="w-60 bg-gray-100 rounded-md p-2"
-          placeholder="Email e.g jon.conor@sky.net" />
-        <input type="text" class="w-60 mt-4 bg-gray-100 rounded-md p-2" />
+          class="p-2 bg-gray-100 rounded-md w-60"
+          placeholder="Email address" />
+        <input
+          type="text"
+          placeholder="Password"
+          class="p-2 mt-4 bg-gray-100 rounded-md w-60" />
         <button class="ml-4 bg-fuchsia-500 text-white rounded-md p-2.5">
           <arrow-right-icon class="h-5" />
         </button>
       </div>
     </div>
-    <div class="hidden flex-grow">food</div>
+    <div class="flex-grow hidden">food</div>
   </div>
 </template>
 <script lang="ts">
