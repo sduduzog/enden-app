66ccd954f56e71dd1d05110d5cc97cd5fd9dd476
beautus <gumedesduduzo@gmail.com>
2022-03-21 03:33:17 +0200

    the thing works

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import VueSupabase from 'vue-supabase';
 import App from '~/App.vue';
 import '~/index.css';
 import router from '~/router';
+// import { registerSW } from 'virtual:pwa-register';
 
+// registerSW();
 const app = createApp(App);
 
 app.use(VueSupabase, {
--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,26 @@ export default defineConfig({
     vue(),
     pwa({
       registerType: 'autoUpdate',
-      includeManifestIcons: false,
       manifest: {
         theme_color: '#FF00FF',
-        icons: [],
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
       },
     }),
   ],
